ebf64479f64fbeab52101fedc2be54c04237946e
Cisco <frandesramos@gmail.com>
2024-07-26 23:50:33 -0400

    Chapter 1: Servers - Lesson 5: Fileservers

--- main.go
+++ main.go
@@ -11,6 +11,8 @@ func (Handler) ServeHTTP(http.ResponseWriter, *http.Request) {}
 func main() {
 	mux := http.NewServeMux()
 
+	mux.Handle("/", http.FileServer(http.Dir(".")))
+
 	server := http.Server{
 		Addr:    "localhost:8080",
 		Handler: mux,
