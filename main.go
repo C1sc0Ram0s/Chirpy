37907daeb11f58dc253c33229e5d1202e9643f8c
Cisco <frandesramos@gmail.com>
2024-07-30 23:58:14 -0400

    Ch2 - Routing, Lesson 3 - Routing

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
@@ -9,26 +10,60 @@ type Handler struct{}
 
 func (Handler) ServeHTTP(http.ResponseWriter, *http.Request) {}
 
+// Add a handler which displays status
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
+}
+
+// ApiConfig holds our application state
+type ApiConfig struct {
+	fileserverHits int
+}
+
+// Middleware function to increment request count
+func (cfg *ApiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		cfg.fileserverHits++
+
+		// Call the next handler
+		next.ServeHTTP(w, req)
+	})
+}
+
+// Add a handler which displays metrics
+func (cfg *ApiConfig) handlerMetrics(w http.ResponseWriter, req *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileserverHits)))
+}
+
+// Add a handler which resets the metrics
+func (cfg *ApiConfig) handlerReset(w http.ResponseWriter, req *http.Request) {
+	cfg.fileserverHits = 0
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Hits reset to 0"))
+}
+
 func main() {
 	const filepathRoot = "."
 	const port = "8080"
+	var apiCfg = &ApiConfig{}
 
-	mux := http.NewServeMux()
-	mux.Handle("/app/*", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
-	mux.HandleFunc("/healthz", handlerReadiness)
+	router := http.NewServeMux()
+	router.Handle("/app/*", http.StripPrefix("/app", apiCfg.middlewareMetricsInc(http.FileServer(http.Dir(filepathRoot)))))
+	router.HandleFunc("GET /healthz", handlerReadiness)
+	router.HandleFunc("GET /metrics", apiCfg.handlerMetrics)
+	router.HandleFunc("/reset", apiCfg.handlerReset)
 
 	server := http.Server{
 		Addr:    ":" + port,
-		Handler: mux,
+		Handler: router,
 	}
 
 	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	log.Fatal(server.ListenAndServe())
 
-}
-
-func handlerReadiness(w http.ResponseWriter, r *http.Request) {
-	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(http.StatusText(http.StatusOK)))
 }
