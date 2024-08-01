b7e213c66bb5dc84708560445ae3f4eda4accacc
Cisco <frandesramos@gmail.com>
2024-07-31 22:08:57 -0400

    Ch 4: JSON - Lesson 2: JSON && Project Restructuring

--- main.go
+++ main.go
@@ -1,76 +1,89 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"log"
 	"net/http"
 )
 
-type Handler struct{}
-
-func (Handler) ServeHTTP(http.ResponseWriter, *http.Request) {}
-
-// Add a handler which displays status
-func handlerReadiness(w http.ResponseWriter, r *http.Request) {
-	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(http.StatusText(http.StatusOK)))
-}
-
-// ApiConfig holds our application state
-type ApiConfig struct {
+type apiConfig struct {
 	fileserverHits int
-}
-
-// Middleware function to increment request count
-func (cfg *ApiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-		cfg.fileserverHits++
-
-		// Call the next handler
-		next.ServeHTTP(w, req)
-	})
-}
-
-// Add a handler which displays metrics
-func (cfg *ApiConfig) handlerMetrics(w http.ResponseWriter, req *http.Request) {
-	w.Header().Add("Content-Type", "text/html; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(fmt.Sprintf(`
-	<html>
-	<body>
-		<h1>Welcome, Chirpy Admin</h1>
-		<p>Chirpy has been visited %d times!</p>
-	</body>
-	</html>
-	`, cfg.fileserverHits)))
-}
-
-// Add a handler which resets the metrics
-func (cfg *ApiConfig) handlerReset(w http.ResponseWriter, req *http.Request) {
-	cfg.fileserverHits = 0
-	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("Hits reset to 0"))
 }
 
 func main() {
 	const filepathRoot = "."
 	const port = "8080"
-	var apiCfg = &ApiConfig{}
+
+	apiCfg := apiConfig{
+		fileserverHits: 0,
+	}
 
 	router := http.NewServeMux()
-	router.Handle("/app/*", http.StripPrefix("/app", apiCfg.middlewareMetricsInc(http.FileServer(http.Dir(filepathRoot)))))
-	router.HandleFunc("GET /api/healthz", handlerReadiness)
-	router.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
-	router.HandleFunc("/api/reset", apiCfg.handlerReset)
+	fsHandler := apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+	router.Handle("/app/*", fsHandler)
 
-	server := http.Server{
+	router.HandleFunc("GET /api/healthz", handlerReadiness)
+	router.HandleFunc("GET /api/reset", apiCfg.handlerReset)
+	router.HandleFunc("POST /api/validate_chirp", handlerChirpsValidate)
+
+	router.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
+
+	server := &http.Server{
 		Addr:    ":" + port,
 		Handler: router,
 	}
 
 	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	log.Fatal(server.ListenAndServe())
+}
 
+func handlerChirpsValidate(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		Body string `json:"body"`
+	}
+	type returnVals struct {
+		Valid bool `json:"valid"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't decode parameters")
+		return
+	}
+
+	const maxChirpLength = 140
+	if len(params.Body) > maxChirpLength {
+		respondWithError(w, http.StatusBadRequest, "Chirp is too long")
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, returnVals{
+		Valid: true,
+	})
+}
+
+func respondWithError(w http.ResponseWriter, code int, msg string) {
+	if code > 499 {
+		log.Printf("Responding with 5XX error: %s", msg)
+	}
+	type errorResponse struct {
+		Error string `json:"error"`
+	}
+	respondWithJSON(w, code, errorResponse{
+		Error: msg,
+	})
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	dat, err := json.Marshal(payload)
+	if err != nil {
+		log.Printf("Error marshalling JSON: %s", err)
+		w.WriteHeader(500)
+		return
+	}
+	w.WriteHeader(code)
+	w.Write(dat)
 }
